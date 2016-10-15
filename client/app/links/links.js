9e5c3e997d8a1ee6ee75674b9c3f8e6d3543b0ea
jessemavi <jesse.mavi@gmail.com>
2016-10-14 20:05:20 -0700

    Pass linksControllerSpec, servicesSpec, and shortenControllerSpec

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,14 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  $scope.data = {};
+  $scope.getAll = function() {
+    // populate data object
+    Links.getAll()
+      .then(function(links) {
+        $scope.data['links'] = links;
+      });
+  };
+
+  $scope.getAll();
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,4 +2,14 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
+  $scope.link = {};
+  $scope.addLink = function() {
+    Links.addOne()
+      .then(function(link) {
+        $scope.link['url'] = $location.path('/link');
+      });
+  };
+
 });
+
+
